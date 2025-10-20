c99f34dc01a4f20084dadada26ed8ccabd77be1b
Eilen Fonseca Garrote <laneko27@gmail.com>
2025-10-20 14:41:28 +0000

    Upload New File

---
+++ src/app/interruptos/interruptosActions.tsx
@@ -0,0 +1,25 @@
+import { Button } from "@/components/ui/button";
+
+/* acciones para descargar pdf y calcular claves de ausentismo*/
+interface InterruptosActionsProps {
+  onCalculate: () => void;
+  onDownload: (e: React.MouseEvent<HTMLAnchorElement>) => void;
+} 
+
+export default function InterruptosActions({
+  onCalculate,
+  onDownload,
+}: InterruptosActionsProps) {
+  return (
+    <div className="flex justify-between">
+      <Button onClick={onCalculate}>Calcular</Button>
+      <a
+        href="#"
+        onClick={onDownload}
+        className="bg-red-600 text-white px-4 py-2 rounded-lg hover:bg-red-700 transition flex items-center gap-2"
+      >
+        Descargar <i className="fa fa-file-pdf" />
+      </a>
+    </div>
+  );
+}
