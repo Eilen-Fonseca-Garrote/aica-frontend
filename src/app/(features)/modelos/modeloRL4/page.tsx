093d2fdaedc9be7c1406ad8fa4032531ae97060e
Eilen Fonseca Garrote <laneko27@gmail.com>
2025-11-11 05:32:01 +0000

    fixed:Unused values on Modelo-RL4

--- src/app/(features)/modelos/modeloRL4/page.tsx
+++ src/app/(features)/modelos/modeloRL4/page.tsx
@@ -1,6 +1,3 @@
-import Link from "next/link"
-import { Download, Minus, ArrowLeft } from "lucide-react"
-import { Button } from "@/components/ui/button"
 import ModeloRl4Form from "./exportarExcel"
 
 export default function ModeloRl4Page() {
