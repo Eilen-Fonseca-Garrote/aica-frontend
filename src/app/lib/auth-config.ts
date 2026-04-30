630a333cbbd8f3ee70ba29565b01db2e964fa4a5
PequeWatson <sosa92979@gmail.com>
2026-04-30 02:04:32 +0200

    feat: add default true to NEXT_PUBLIC_DISABLE_AUTH_IN_DEV

--- src/app/lib/auth-config.ts
+++ src/app/lib/auth-config.ts
@@ -1,3 +1,1 @@
-export const AUTH_DISABLED_IN_DEV =
-  process.env.NODE_ENV === 'development' &&
-  process.env.NEXT_PUBLIC_DISABLE_AUTH_IN_DEV === 'true';
+export const AUTH_DISABLED_IN_DEV = (process.env.NEXT_PUBLIC_DISABLE_AUTH_IN_DEV ?? 'true') === 'true';
