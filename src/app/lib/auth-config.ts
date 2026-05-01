a6ad3f8ebcdc72dfd15535a815c394a65c0cc627
Rodny Estrada <rrodnyestrada1@gmail.com>
2026-04-30 23:09:21 -0400

    chore: enable auth option

--- src/app/lib/auth-config.ts
+++ src/app/lib/auth-config.ts
@@ -1,1 +1,1 @@
-export const AUTH_DISABLED_IN_DEV = (process.env.NEXT_PUBLIC_DISABLE_AUTH_IN_DEV ?? 'true') === 'true';
+export const AUTH_DISABLED_IN_DEV = (process.env.NEXT_PUBLIC_DISABLE_AUTH_IN_DEV ?? 'false') === 'true';
